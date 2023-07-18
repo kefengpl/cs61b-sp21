499378ea8c8b392a121408b96b81d9b336bbbd11
kefengpl <1335508895@qq.com>
2023-07-18 11:27:33 +0800

    just for test

---
+++ proj0/game2048/________test.java
@@ -0,0 +1,7 @@
+package game2048;
+public class ________test {
+    public static void main(String[] args) {
+        Tile newtile = Tile.create(15, 1, 2);
+        System.out.println(newtile.col());
+    }
+}
