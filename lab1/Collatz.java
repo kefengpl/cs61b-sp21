777d0cdcf9d28b1d741d2a5c24a9454772bb1198
kefengpl <1335508895@qq.com>
2024-01-30 19:03:33 +0800

    lab 4b

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -8,12 +8,13 @@ public class Collatz {
      * if n is even, then return n / 2
      * */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        }
+        if (n % 2 == 0) {
+            return n / 2;
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
